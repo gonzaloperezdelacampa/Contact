b5a0ad69fa9647724288256fc8b28e68768c2382
Gonzalo Perez <gpcampa@yahoo.es>
2026-08-03 11:31:26 +0200

    stage

--- src/com/lcdi/contact/Contacts.java
+++ src/com/lcdi/contact/Contacts.java
@@ -5,10 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "contacts")
